f87fa10296a1a6a84eb2fe723c3c124fdfb56cc4
Александр Загородний <git.hub.zagar@gmail.com>
2023-09-08 15:13:49 +0200

    create book entity , implemented service and repository levels

--- src/main/java/book/store/OnlineBookStoreApplication.java
+++ src/main/java/book/store/OnlineBookStoreApplication.java
@@ -1,13 +1,35 @@
 package book.store;
 
+import book.store.model.Book;
+import book.store.service.BookService;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
+    @Autowired
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book kobzar = new Book();
+            kobzar.setTitle("Kobzar");
+            kobzar.setAuthor("Taras Shevchenko");
+            kobzar.setIsbn("1122");
+            kobzar.setPrice(BigDecimal.valueOf(120));
+
+            bookService.save(kobzar);
+            System.out.println(bookService.fingAll());
+        };
     }
 
 }
---
+++ src/main/java/book/store/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,47 @@
+package book.store.repository.impl;
+
+import book.store.exception.DataProcessingException;
+import book.store.model.Book;
+import book.store.repository.BookRepository;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Transaction transaction = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save " + book + " to DB", e);
+        }
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Book ").getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find books from DB", e);
+        }
+    }
+}
